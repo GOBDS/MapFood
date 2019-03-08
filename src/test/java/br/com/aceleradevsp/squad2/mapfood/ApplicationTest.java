730871bf1add65834e57549668489f19aa6caec3
dritoferro <adrianotagliaferro@hotmail.com>
2019-03-07 22:58:47 -0300

    Code review.

--- src/test/java/br/com/aceleradevsp/squad2/mapfood/ApplicationTest.java
+++ src/test/java/br/com/aceleradevsp/squad2/mapfood/ApplicationTest.java
@@ -1,6 +1,5 @@
 package br.com.aceleradevsp.squad2.mapfood;
 
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -8,7 +7,5 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ApplicationTest {
-    @Test
-    public void contextLoads() {
-    }
+
 }
