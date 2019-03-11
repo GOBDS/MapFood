89cf96ababdd4c7fe80b50eb692697f7881929be
vntgasi <gabriel.silva@venturus.org.br>
2019-03-11 09:48:07 -0300

    Fix: Increase test coverage

--- src/test/java/br/com/aceleradevsp/squad2/mapfood/MotoboyIntegrationTest.java
+++ src/test/java/br/com/aceleradevsp/squad2/mapfood/MotoboyIntegrationTest.java
@@ -1,17 +1,20 @@
 package br.com.aceleradevsp.squad2.mapfood;
 
+import br.com.aceleradevsp.squad2.mapfood.logistic.LogisticService;
 import br.com.aceleradevsp.squad2.mapfood.logistic.MotoboyModel;
 import br.com.aceleradevsp.squad2.mapfood.logistic.MotoboyRepository;
 import br.com.aceleradevsp.squad2.mapfood.motosimulator.SchedulerMotoboy;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.geo.*;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.time.Duration;
-import java.util.List;
+import java.time.Instant;
 
 import static org.hamcrest.core.Is.is;
 import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
@@ -22,22 +25,17 @@ import static org.junit.Assert.*;
 public class MotoboyIntegrationTest {
 
     @Autowired
-    MotoboyRepository repository;
+    MotoboyRepository motoboyRepository;
+
+    @Autowired
+    LogisticService service;
 
     @Autowired
     SchedulerMotoboy schedulerMotoboy;
 
-
-    @Test
-    public void assertMotoboyPopulator(){
-        List<MotoboyModel> all = repository.findAll();
-        assertNotNull(all);
-    }
-
     @Test
     public void assertNearMotoboy(){
-        Point point = new Point(-30.03742831, -51.228496);
-        GeoResults<MotoboyModel> byPositionNear = repository.findByPositionNear(point, new Distance(5, Metrics.KILOMETERS));
+        GeoResults<MotoboyModel> byPositionNear = service.getNearestMotoboy(-30.03742831, -51.228496);
         assertNotNull(byPositionNear);
         assertTrue("Assert quantity of near motoboys",byPositionNear.getContent().size() >= 1);
     }
@@ -48,8 +46,8 @@ public class MotoboyIntegrationTest {
         position[0] = -25.03742831;
         position[1] = -52.21598075;
         schedulerMotoboy.schedule(1, position[0], position[1], Duration.ofMillis(10L));
-        Thread.sleep(80L);
-        MotoboyModel motoboyModel = repository.findById(1).orElseThrow(NullPointerException::new);
+        Thread.sleep(100L);
+        MotoboyModel motoboyModel = motoboyRepository.findById(1).orElseThrow(NullPointerException::new);
         assertThat(motoboyModel.getPosition(), is(position));
     }
 }
