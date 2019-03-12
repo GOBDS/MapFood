96b2cf02349c2fe6501f3adf3184777675e5cbe9
dritoferro <adrianotagliaferro@hotmail.com>
2019-03-12 13:39:46 -0300

    Code review.

--- src/main/java/br/com/aceleradevsp/squad2/mapfood/order/OrderRepository.java
+++ src/main/java/br/com/aceleradevsp/squad2/mapfood/order/OrderRepository.java
@@ -1,6 +1,8 @@
 package br.com.aceleradevsp.squad2.mapfood.order;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OrderRepository extends MongoRepository<OrderModel, String> {
 }
